0e462e917ea3a78f2e843978deb3c712730b5544
JIBANJYOTIMOHANTY <jibanjyotimohanty315@gmail.com>
2024-07-27 18:38:04 +0530

    "Updated import statement and replaced React fragments with explicit React.Fragment tags"

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Nav from './components/Nav'
 import About from './components/About'
@@ -8,14 +8,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <React.Fragment >
       <div className='h-screen w-screen bg-[#2F2F2F]'>
         <Nav />
         <About/>
         
       </div>
       {/* <h1 className='text-red-600'>hello</h1> */}
-    </>
+    </React.Fragment>
   )
 }
 
