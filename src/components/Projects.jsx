42054fde58a63660796a4682aaa43cb5153ea596
JIBANJYOTIMOHANTY <jibanjyotimohanty315@gmail.com>
2024-08-22 10:40:50 +0530

    "Removed unnecessary 'ref' prop from inner div and added it to outer div in Projects component."

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,9 +6,9 @@ import { ProjectData } from '../data/ProjectData'
 
 const Projects = forwardRef((props,ref) => {
   return (
-    <div >
+    <div ref={ref}>
       <h1 className='font-semibold text-center mt-5 text-black text-3xl md:text-4xl lg:text-5xl font-poppins underline md:mt-10 md:ml-4'>Projects</h1>
-      <div ref={ref} className='md:grid md:grid-cols-3 md:mr-5'>
+      <div  className='md:grid md:grid-cols-3 md:mr-5'>
         {ProjectData.map((item, index) =>
           <div className=' rounded-xl m-5 hover:shadow-lg transition-shadow duration-300 ease-linear hover:shadow-white cursor-pointer' key={index}>
             <h4 className='font-poppins font-bold text-2xl text-white text-center'>{item.heading}</h4>
