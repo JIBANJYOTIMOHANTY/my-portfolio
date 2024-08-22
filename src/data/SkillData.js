4250762b5d3798076ba26d545c37d04ab268a218
JIBANJYOTIMOHANTY <jibanjyotimohanty315@gmail.com>
2024-08-22 10:34:40 +0530

    "Added docker.svg to SkillData.js"

--- src/data/SkillData.js
+++ src/data/SkillData.js
@@ -12,7 +12,7 @@ import github from "../images/skills/github.svg"
 import vscode from "../images/skills/vscode.svg"
 import intellij from "../images/skills/intellij.svg"
 import postgresql from "../images/skills/postgresql.svg"
-
+import docker from "..images/skills/docker.svg"
 export const language = [
     
     {
